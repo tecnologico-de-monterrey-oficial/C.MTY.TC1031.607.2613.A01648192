175895ca0f952f62ec4c19703892eb164f8f8d89
Mich <michi.lakess@gmail.com>
2026-08-16 18:21:56 -0600

    act1.1

--- Assignments/Act1.1-Templates/List.h
+++ Assignments/Act1.1-Templates/List.h
@@ -1,7 +1,8 @@
 #ifndef LIST_H
 #define LIST_H
 #include <vector>
-//using namespace std;
+#include <iostream>
+using namespace std;
 
 template <typename T>
 class List{
@@ -21,8 +22,8 @@ class List{
         
 };
 
-//no me gusta poner código extra en el archivo h, entonces moví las cosas que pusimos en clase al archivo de Actividad1.1...
-/*
+//FUNCS
+
 template <typename T>
 List<T>::List(){
     size=0;
@@ -35,12 +36,68 @@ void List<T>::insert(T data){
 }
 
 template <typename T>
-void List<T>::print(){
-    std::cout<<"[  ";
-    for(int i=0;i<size;i++){
-        std::cout<<list[i]<<"  ";
+void List<T>::removeLast(){
+    if(size==0){
+        cout<<"NO HAY ELEMENTOS"<<endl;
+    }else{
+        cout<<list[size-1]<<endl;
+        list.pop_back();
+        size--;
     }
-    std::cout<<"]"<<std::endl;
 }
-*/
+
+template <typename T>
+T List<T>::getData(int pos){
+    return list[pos];
+}
+
+template <typename T>
+int List<T>::getSize(){
+    return size;
+}
+
+template <typename T>
+T List<T>::getMax(){
+    if(size==0){
+        return T();//vació para que regrese
+    }
+    T maxVal=list[0];
+    for(int i=1;i<size;i++){
+        if (list[i]>maxVal){
+            maxVal=list[i];
+        }
+    }
+    return maxVal;
+}
+
+template <typename T>
+void List<T>::print(){
+    for(int i=0;i<size;i++){
+        cout<<"["<<i<<"] - "<<list[i]<<endl;
+    }
+}
+
+template <typename T>
+void List<T>::insertAt(int pos, T data){
+    if(pos<0 || pos>size){
+        cout<<"POSICIÓN INVÁLIDA"<<endl; //le pongo tilde pq así decía en canvas pero quizás no salgan bien
+    }else{
+        list.insert(list.begin()+pos, data);
+        size++;
+    }
+}
+
+template <typename T>
+void List<T>::removeAt(int pos){
+    if(size==0){
+        cout<<"NO HAY ELEMENTOS"<<endl;
+    }else if(pos<0 || pos>= size){
+        cout<<"POSICIÓN INVÁLIDA"<<endl;
+    }else{
+        cout<<list[pos]<<endl;
+        list.erase(list.begin()+pos);
+        size--;
+    }
+}
+
 #endif
--- Assignments/Act1.1-Templates/act1.1-Templates.cpp
+++ Assignments/Act1.1-Templates/act1.1-Templates.cpp
@@ -16,89 +16,10 @@ string sum(string a, string b){
 }
 */
 template <typename T>
-List<T>::List(){
-    size=0;
+T sum(T a, T b) {
+    return a + b;
 }
 
-template <typename T>
-void List<T>::insert(T data){
-    list.push_back(data);
-    size++;
-}
-
-template <typename T>
-void List<T>::removeLast(){
-    if(size==0){
-        cout<<"NO HAY ELEMENTOS"<<endl;
-    }else{
-        cout<<list[size-1]<<endl;
-        list.pop_back();
-        size--;
-    }
-}
-
-template <typename T>
-T List<T>::getData(int pos){
-    return list[pos];
-}
-
-template <typename T>
-int List<T>::getSize(){
-    return size;
-}
-
-template <typename T>
-T List<T>::getMax(){
-    if(size==0){
-        return T();//vació para que regrese
-    }
-    T maxVal=list[0];
-    for(int i=1;i<size;i++){
-        if (list[i]>maxVal){
-            maxVal=list[i];
-        }
-    }
-    return maxVal;
-}
-
-template <typename T>
-void List<T>::print(){
-    for(int i=0;i<size;i++){
-        cout<<"["<<i<<"], "<<list[i]<<endl;
-    }
-}
-
-template <typename T>
-void List<T>::insertAt(int pos, T data){
-    if(pos<0 || pos>size){
-        cout<<"POSICIÓN INVÁLIDA"<<endl; //le pongo tilde pq así decía en canvas pero quizás no salgan bien
-    }else{
-        list.insert(list.begin()+pos, data);
-        size++;
-    }
-}
-
-template <typename T>
-void List<T>::removeAt(int pos){
-    if(size==0){
-        cout<<"NO HAY ELEMENTOS"<<endl;
-    }else if(pos<0 || pos>= size){
-        cout<<"POSICIÓN INVÁLIDA"<<endl;
-    }else{
-        cout<<list[pos]<<endl;
-        list.erase(list.begin()+pos);
-        size--;
-    }
-}
-
-/* sum no se usa según lo que pide el profe pero no borro nada por si las moscas
-template<typename T> 
-T sum(T a, T b){
-    return a+b;
-}*/
-
-
-/*
 int main(){
     string a="hola";
     string b="crayola";
@@ -129,9 +50,5 @@ int main(){
     cout<<"strings: "<<endl;
     things.print();
 
-
-
-
     return 0;
 }
-*/
