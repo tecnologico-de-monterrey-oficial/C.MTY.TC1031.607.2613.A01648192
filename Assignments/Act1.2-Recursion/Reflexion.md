e196ab9a467ed49de5b43db0ea1c1f0da79feecf
Mich <michi.lakess@gmail.com>
2026-08-20 11:11:23 -0600

    act1.2

---
+++ Assignments/Act1.2-Recursion/Reflexion.md
@@ -0,0 +1,8 @@
+¿En qué casos notaste que la versión recursiva fue más lenta o usó más memoria que la iterativa? ¿A qué se debió?
+    hacía la iterativa y la recursiva antes de compilar para comparar resultados, entonces no pude comparar bien cuál era la diferencia de compilación. Sin embargo, si sé que mi laptop se tardó un poco más de lo normal para cargar el último ejercicio, el del exponenete. Se debe a la explicación que vimos en clase: Se están cargando todos los "loops" de la recursividad y los tiene que guardar todos y luego hacer la multiplicación deseada.
+
+Para la suma 1..n, sumFormula resuelve en un solo paso lo que a sumIterative y sumRecursive les toma n pasos. ¿Qué te dice esto sobre buscar una fórmula antes de escribir código?
+    Me dice que tenemos que ser conscientes de los recursos posiblemente disponibles que pueden ser de ayuda a nuestros códigos y trabajos.
+
+Si bacteriasRecursive tuviera que calcular n = 100,000 días, ¿qué problema esperarías encontrar y cómo lo resolverías?
+    Lo ingresé a mi laptop y resultó no darme ningún resultado, dejando un espacio en blanco. Es problable que sea porque llegó al límite que puede ejecutar. La verdad, la única solución que le veo es usar solo la versión iterativa del proceso o confinarse a números mucho más pequeños.
--- Assignments/Act1.2-Recursion/act-1.2-Recursividad.cpp
+++ Assignments/Act1.2-Recursion/act-1.2-Recursividad.cpp
@@ -46,7 +46,61 @@ int fibonacciRecursive(int n){
     return fibonacciRecursive(n-1)+fibonacciRecursive(n-2);
 }
 
+int bacteriaIterative(int n) {
+    double born = 3.78;
+    double died = 2.34;
+    int sum = 1;
+    for (int i=1; i<=n; i++) {
+        int bacBorn = sum * born;
+        int bacDied = sum * died; 
+        sum += bacBorn - bacDied; 
+    }
+    return sum;
 
+}
+
+int bacteriaRecursive(int n) {
+    if (n==0) {
+        return 1;
+    }
+    int bacteria = bacteriaRecursive(n-1);
+    int born = bacteria * 3.78;
+    int died = bacteria * 2.34;
+    return bacteria + born - died;
+}
+
+double investmentIterative(int n, int m){
+    double invest=m;
+    for(int i=1;i<=n;i++){
+        float rate=1.1875;
+        invest= invest*rate;
+    }
+    return invest;
+}
+
+double investmentRecursive(int n, int m){
+    if (n==0){
+        return m;
+    }
+    double invest=investmentRecursive(n-1, m);
+    return invest*1.1875;
+}
+
+int powIterative(int n, int y){
+    int power=1;
+    for(int i=1;i<=y;i++){
+        power*=n;
+    }
+    return power;
+}
+
+int powRecursive(int n, int y){
+    if(y==0){
+        return 1;
+    }
+    int power=powRecursive(n,y-1);
+    return (power*n);
+}
 
 int main(){
     cout<<"La suma iterativa de 5 es: "<<sumIterative(5)<<endl; 
@@ -54,6 +108,12 @@ int main(){
     cout<<"La suma con formula de 5 es: "<<sumFormula(5)<<endl;
     cout<<"La secuencia fibonacci iterativa de 5 es: "<<fibonacciIterative(5)<<endl;
     cout<<"La secuencia fibonacci recursiva de 5 es: "<<fibonacciRecursive(5)<<endl;
+    cout << "La bacteraia iterativa de 5 es: " << bacteriaIterative(5) << endl;
+    cout << "La bacteria recursiva de 5 es: " << bacteriaRecursive(5) << endl;
+    cout << "La inversión iterativa de 100 dolares en 5 meses es: " << investmentIterative(5,100) << endl;
+    cout << "La inversión recursiva de 100 dolares en 5 meses es: " << investmentRecursive(5,100) << endl;
+    cout << "La exponencial iterativa de 5 a la 10 es: " << powIterative(5,10) << endl;
+    cout << "La exponencial recursiva de 5 a las 10 es: " << powRecursive(5,10) << endl;
     return 0;
    
 }
