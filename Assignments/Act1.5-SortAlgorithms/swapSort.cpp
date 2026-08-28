f2fd80e75bc77db05ada1a5bc9e7ca98336dea99
Mich <michi.lakess@gmail.com>
2026-08-28 16:54:26 -0600

    swaps

---
+++ Assignments/Act1.5-SortAlgorithms/swapSort.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+template <typename T>
+void swapS(vector<T> &list){
+    for (int i=0;i<list.size()-1;i++){
+        for(int j=i+1;j<(list.size());j++){
+            if (list[i]>list[j]){
+                swap(list[i],list[j]);
+            }
+        }
+    }
+}
+
+
+template <typename T>
+void bubbleS(vector<T> &list){
+    for (int i=0;i<list.size()-1;i++){
+        for(int j=0;j<(list.size()-1);j++){
+            if (list[j]>list[j+1]){
+                swap(list[j],list[j+1]);
+            }
+        }
+    }
+}
+
+template <typename T>
+void printVec(vector<T> &list){
+    for (int i=0;i<list.size();i++){
+        cout<<list[i]<<", ";
+    }
+}
+
+int main(){
+    vector<int> myList1={15, 7, 3, 9, 12, 5, 2};
+    swapS(myList1);
+
+    vector<int> myList2={15, 7, 3, 9, 12, 5, 2};
+    bubbleS(myList2);
+    printVec(myList1);
+    printVec(myList2);
+    return 0;
+}
+//primer loop para hacer swap, segundo para checar el menor
+//15, 7, 3, 9, 12, 5, 2
