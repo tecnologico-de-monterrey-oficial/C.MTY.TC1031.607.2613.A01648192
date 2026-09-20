2fcf4e9ce10f15498ccdf22784f749f769f5532d
Mich <michi.lakess@gmail.com>
2026-09-20 15:42:47 -0600

    Sorting complete

--- Evidences/Evidence1/main.cpp
+++ Evidences/Evidence1/main.cpp
@@ -8,9 +8,10 @@
 using namespace std;
 
 void dataLogs(vector<Logs>& logs, const string& archivo){
+    logs.clear();//sugerencia de gemini para que el vector no duplique los datos en reruns
     ifstream file(archivo);
     if (!file.is_open()){
-        cout<<"No se pudo abrir archivo."<<archivo<<endl;
+        cout<<"No se pudo abrir archivo "<<archivo<<endl;
         return;
     }
     string line;
@@ -22,35 +23,58 @@ void dataLogs(vector<Logs>& logs, const string& archivo){
     file.close();
 }
 
-void saveLogs(const vector<Logs>& logs, const string& archivo){
+//exportar el sorting más reciente al mismo archivo
+void saveLogs(const vector<Logs>& logs){
+    string archivo= "output608.txt";
     ofstream file(archivo);
     for (int i=0; i<logs.size(); i++){
         file<<logs[i]<<endl;
     }
     file.close();
-    cout<<"Archivo: "<<archivo<<endl;
+    cout<<"Nuevo archivo ordenado: "<<archivo<<endl;
 }
 
 int main(){
     cin.exceptions(ios::failbit | ios::badbit);
-
-    vector<Logs> original;
-    dataLogs(original, "log607-1.txt");
-
-    cout<<"Numero de registro: "<<original.size()<<endl;
-
-    int opcion= 0;
+    bool repetir= true;
     long long comps= 0;
     long long swaps= 0;
-
-    do{
+    while (repetir){
+        int chooseArchivo= 0; 
         bool opcionValida= false;
+        do{
+            try{
+                cout<<"Elige una base de datos"<<endl;
+                cout<<"1. log607-1txt (database desordenada)"<<endl;
+                cout<<"2. log607-2.txt (database casi ordenada)"<<endl;
+                cin>>chooseArchivo;
+                if (chooseArchivo==1 || chooseArchivo==2){
+                    opcionValida= true;
+                }else{
+                    cout<<"Selecciona 1 o 2."<<endl;
+                }
+            }catch(ios_base::failure& ex){
+                cout<<"Error, selecciona un valor valido"<<endl;
+                cin.clear();
+                cin.ignore(1000, '\n');
+            }
+        }while (!opcionValida); //no se porque pero no le agarrab a esta parte de la logica, gemini me ayudo con lo de opcionValida
+        string nombreArchivo= (chooseArchivo== 1)?"log607-1.txt": "log607-2.txt";
+
+        vector<Logs> logsSortear;
+        cout<<"Sorting: "<<nombreArchivo<<endl;
+        cout<<"Cantidad de logs: "<<logsSortear.size()<<endl;;
+        dataLogs(logsSortear, nombreArchivo);
+
+        //segundo menu, algoritmos, lo que ya tenia
+        int opcionAlgo= 0;
+        opcionValida= false;
         do{
             try{
                 cout<<"\nElige uno para ordenar los logs"<<endl;
                 cout<<"1. Swap Sort\n2. Bubble Sort\n3. Selection Sort\n4. Insertion Sort" << endl;
                 cout<<"5. Merge Sort\n6. Quick Sort\n7. Shell Sort\n9. Salir\n> ";
-                cin>>opcion;
+                cin>>opcionAlgo;
                 opcionValida= true;
             }catch(ios_base::failure& ex){
                 cout<<"Error. Elige del 1 al 9"<<endl;
@@ -58,65 +82,101 @@ int main(){
                 cin.ignore(1000, '\n');
             }
         }while (!opcionValida);
-        if (opcion>=1 && opcion<=7){
-            //crear copia de los unsorted
-            vector<Logs> logsSortear= original;
-            string nombreAlgoritmo= "";
-            
-            cout <<"\nOrdenando los"<<logsSortear.size()<<"datos."<<endl;
-            auto start= chrono::high_resolution_clock::now();
+        cin.ignore(1000, '\n'); //otra sugerencia de Gemini, para limpiar la basura del buffer en preparacion a los siguientes inputs
+        
+        //siguiente menu, predicciones de usuario
+        string prediccion;
+        cout<<"Que tan rapido esperas que sea el sorting con las opciones elegidas?"<<endl;
+        getline(cin, prediccion);//preguntar
+        string nombreAlgoritmo= "";
+        string complejidad= "";
 
-            switch (opcion) {
+        auto start= chrono::high_resolution_clock::now();
+
+        //info de BigO sacada de https://www.bigocheatsheet.com/
+        switch (opcionAlgo) {
                 case 1:
                     nombreAlgoritmo= "Swap_Sort";
+                    complejidad= "Mejor caso: O(n^2), peor caso: O(n^2)";
                     swapS(logsSortear, comps, swaps); 
                     break;
                 case 2:
                     nombreAlgoritmo= "Bubble_Sort";
+                    complejidad= "Mejor caso: O(n), peor caso: O(n^2)";
                     bubbleS(logsSortear, comps, swaps); 
                     break;
                 case 3:
                     nombreAlgoritmo= "Selection_Sort";
+                    complejidad= "Mejor caso: O(n^2), peor caso: O(n^2)";
                     selectionS(logsSortear, comps, swaps); 
                     break;
                 case 4:
                     nombreAlgoritmo= "Insertion_Sort";
+                    complejidad= "Mejor caso: O(n), peor caso: O(n^2)";
                     insertS(logsSortear, comps, swaps); 
                     break;
                 case 5:
                     nombreAlgoritmo= "Merge_Sort";
+                    complejidad= "Mejor caso: O(n log n), peor caso: O(n log n)";
                     mergeSort(logsSortear); 
                     break;
                 case 6:
                     nombreAlgoritmo= "Quick_Sort";
+                    complejidad= "Mejor caso: O(n log n), peor caso: O(n^2)";
                     quickS(logsSortear); 
                     break;
                 case 7:
                     nombreAlgoritmo= "Shell_Sort";
+                    complejidad= "Mejor caso: O(n log n), peor caso: O(n^2)";
                     shellS(logsSortear); 
                     break;
-            }
-
-            auto end= chrono::high_resolution_clock::now();
-            auto millis= chrono::duration_cast<chrono::milliseconds>(end-start).count();
-
-            cout<<"Algoritmo: "<<nombreAlgoritmo<<endl;
-            cout<<"Tiempo de ejecucion: "<<millis<<" ms\n";
-
-            if (opcion <= 4) {
-                cout<<"Comparaciones: "<<comps<<"\nIntercambios: "<<swaps<<"\n";
-            }
-
-            //gardar a archivo
-            string outputFile= "ordenado_"+ nombreAlgoritmo+ ".txt";
-            saveLogs(logsSortear, outputFile);
-
-        } else if (opcion != 9) {
-            cout<<"Opcion no valida."<<endl;
         }
 
-    }while(opcion != 9);
+        auto end= chrono::high_resolution_clock::now();
+        auto millis= chrono::duration_cast<chrono::milliseconds>(end-start).count();
 
+        //resultados
+        cout<<"\nResultados del sorting:"<<endl;
+        cout<<"Algoritmo: "<< nombreAlgoritmo<<endl;
+        cout<<"Database usado: "<<nombreArchivo<<", de tama;o: "<<logsSortear.size()<<endl;
+        cout<<"Tiempo de ejecucion: "<<millis<<" ms"<<endl;
+        //comparacion?
+        cout<<"Tu prediccion fue: '"<<prediccion<<"'"<<endl;
+
+        if (opcionAlgo<=4){
+            cout<<"Comparaciones: "<<comps<<"\nIntercambios: "<<swaps<<endl;
+        }
+        cout<<"complejidad: "<<complejidad<<endl;
+
+        saveLogs(logsSortear);
+
+        //repetir?
+        int respuesta= 0;
+        opcionValida= false;
+        do{
+            try{
+                cout<<"Quieres volver a sortear? \n1= si, 2= no"<<endl;
+                cin>>respuesta;
+                if(respuesta== 1 || respuesta== 2){
+                    opcionValida= true;
+                }else{
+                    cout<<"Error, elige 1 o 2"<<endl;
+                    cin.clear();
+                    cin.ignore(1000, '\n');
+                }
+            }catch(ios_base::failure& ex){
+                cout<<"Error. Elige 1 o 2"<<endl;
+                cin.clear();
+                cin.ignore(1000, '\n');
+            }
+        }while(!opcionValida);
+        if (respuesta== 2){
+            repetir= false;
+        }
+    }
     cout<<"Bye bye"<<endl;
     return 0;
 }
+
+//no me queda completamente claro lo de comparar?
+//falta toda la parte de rangos
